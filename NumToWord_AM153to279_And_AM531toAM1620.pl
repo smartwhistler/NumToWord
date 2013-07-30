87c61d8958e9e7d517eebf6979c292cc8275c43a
Smart Whistler <754505612@qq.com>
2013-07-30 11:47:06 +0800

    修改AM范围

--- NumToWord_AM153to279_And_AM531toAM1620.pl
+++ NumToWord_AM153to279_And_AM531toAM1620.pl
@@ -3,11 +3,15 @@ use warnings;
 use strict;
 use 5.010;
 use encoding 'utf8';
-use Encode;
 use utf8;
 
-# AM: From, 503 To, 1600
-my @am_all = (503..1600);
+# AM: 
+# From 153 To 279 
+# And
+# From 531 To 1620
+my @am_all;
+push @am_all, 153..279;
+push @am_all, 531..1620;
 
 foreach (@am_all) {
 	my @all_version_for_the_num;
@@ -20,43 +24,88 @@ foreach (@am_all) {
 	push(@all_version_for_the_num, $normal);
 
 	my $with_decade_hundred_thousand=$normal;
-	if(503<=$_ && $_<1000) {
+	if(153<=$_ && $_<1000) {
 		# 三位数
 		if($_%100 == 0) {
 			# 整百
 			$with_decade_hundred_thousand =~ s/^(.)零零/$1百/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		} elsif($_%10 == 0) {
 			# 整十
 			$with_decade_hundred_thousand =~ s/^(.)(.)零/$1百$2十/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 			$with_decade_hundred_thousand =~ s/^(.+)十/$1/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		} else {
 			# 普通
 			$with_decade_hundred_thousand =~ s/^(.)(.)(.)$/$1百$2十$3/;
 			$with_decade_hundred_thousand =~ s/百零十/百零/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		}
-	} elsif(1000<=$_ && $_<=1600) {
+	} elsif(1000<=$_ && $_<=1620) {
 		# 四位数
 		if($_%1000 == 0) {
 			# 整千
 			$with_decade_hundred_thousand =~ s/^(.)零零零$/$1千/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		} elsif($_%100 == 0) {
 			# 整百
 			$with_decade_hundred_thousand =~ s/^(.)(.)零零$/$1千$2百/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 			$with_decade_hundred_thousand =~ s/^(.+)百$/$1/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		} elsif($_%10 == 0) {
 			# 整十
 			$with_decade_hundred_thousand =~ s/^(.)(.)(.)零$/$1千$2百$3十/;
 			$with_decade_hundred_thousand =~ s/零百/零/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 			$with_decade_hundred_thousand =~ s/^(.+)十$/$1/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		} else {
 			# 普通
 			$with_decade_hundred_thousand =~ s/^(.)(.)(.)(.)$/$1千$2百$3十$4/;
@@ -64,6 +113,11 @@ foreach (@am_all) {
 			$with_decade_hundred_thousand =~ s/零十/零/;
 			$with_decade_hundred_thousand =~ s/零+/零/;
 			push(@all_version_for_the_num, $with_decade_hundred_thousand);
+			if($with_decade_hundred_thousand =~ m/二百/) {
+				my $tmp = $with_decade_hundred_thousand;
+				$tmp =~ s/二百/两百/;
+				push(@all_version_for_the_num, $tmp);
+			}
 		}
 	} else {
 		die "Error: value ".$_." not right.\n";
@@ -77,5 +131,5 @@ foreach (@am_all) {
 	my %hash = ();
 	@all_version_for_the_num = grep {!$hash{$_}++} @all_version_for_the_num;
 
-	printf("%s\t".("%s\t" x @all_version_for_the_num)."\n", $_, @all_version_for_the_num);
+	printf("%s".("\t%s" x @all_version_for_the_num)."\n", $_, @all_version_for_the_num);
 }
